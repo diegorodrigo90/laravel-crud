fa0949c0ba93df8393218a4e445e3648fbeb2dcb
Diego Rodrigo da Silva <diegors@unicamp.br>
2020-07-01 12:06:27 -0300

    User seed created

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\diego\OneDrive\Documentos\Projetos\vercan\laravel-crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\diego\OneDrive\Documentos\Projetos\vercan\laravel-crud\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\diego\OneDrive\Documentos\Projetos\laravel-crud\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\diego\OneDrive\Documentos\Projetos\laravel-crud\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
