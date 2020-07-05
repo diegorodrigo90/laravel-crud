5d1d1aa839d2c2c27efc28f8ae75adaf35dbec3e
Diego Rodrigo da Silva <diegorodrigo90@gmail.com>
2020-07-05 02:09:01 -0300

    deactivating cities fild while loading state

--- resources/js/fornecedores.js
+++ resources/js/fornecedores.js
@@ -263,7 +263,10 @@ $(document).ready(function() {
     });
 
     $("#uf").change(function() {
+        $("#cidade").prop("disabled", true); //desativando select
+
         $("#cidade").prepend("<option selected disabled>Carregando...</option>"); //exibe carregando na lista de cidades
+
         getCities($(this).val());
     });
 
